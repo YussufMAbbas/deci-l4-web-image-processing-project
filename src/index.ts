7cfb2c402ed0802f5f689c145fb18fe9d71ea11c
Yussuf Mohammad Abbas <yussuf.m.abbas@gmail.com>
2025-05-11 14:50:11 +0300

    Set up main router

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import router from "./routes/index.js";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use("/api", router);
 
 app.listen(port, () => {
     console.log(`Server has started at http://localhost:${port}.`);
