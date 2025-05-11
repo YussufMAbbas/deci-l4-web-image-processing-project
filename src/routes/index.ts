e96e979e7417b83570fa1d0f2137f992c3febe85
Yussuf Mohammad Abbas <yussuf.m.abbas@gmail.com>
2025-05-11 16:25:57 +0300

    Add image resizing route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import resizeImage from "./api/resizeImage.js";
 
 const router = Router();
+
+router.use("/resizeImage", resizeImage);
 
 router.get("/", (req, res) => {
     res.send("Connected to main router.");
